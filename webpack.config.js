b4ea47dc86c628afd495b1823c739913f4925866
Nitish Singh <nitishsingh925@gmail.com>
2024-07-04 22:33:29 +0530

    common js to  module

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+import path from "path";
 
-module.exports = {
+export default {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve("dist"),
   },
   target: "node",
   mode: "development",
