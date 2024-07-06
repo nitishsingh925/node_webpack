b3d557c995ec78c293c5d6941abb02f90e206a92
Nitish Singh <nitishsingh925@gmail.com>
2024-07-06 08:28:21 +0530

    bug fix now

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,10 +1,10 @@
-import path from "path";
+const path = require("path");
 
-export default {
+module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve("dist"),
+    path: path.resolve(__dirname, "dist"),
   },
   target: "node",
   mode: "development",
